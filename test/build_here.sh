7374970800407df7c91e8897f5c0cba02fa3cd6d
Mateusz Ploskon <mploskon@lbl.gov>
2019-06-19 00:36:36 -0700

    fix build

--- test/build_here.sh
+++ test/build_here.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+function os_linux()
+{
+	_system=$(uname -a | cut -f 1 -d " ")
+	if [ $_system == "Linux" ]; then
+		echo "yes"
+	else
+		echo
+	fi
+}
+
+function os_darwin()
+{
+	_system=$(uname -a | cut -f 1 -d " ")
+	if [ $_system == "Darwin" ]; then
+		echo "yes"
+	else
+		echo
+	fi
+}
+
+function n_cores()
+{
+	local _ncores="1"
+	[ $(os_darwin) ] && local _ncores=$(system_profiler SPHardwareDataType | grep "Number of Cores" | cut -f 2 -d ":" | sed 's| ||')
+	[ $(os_linux) ] && local _ncores=$(lscpu | grep "CPU(s):" | head -n 1 | cut -f 2 -d ":" | sed 's| ||g')
+	#[ ${_ncores} -gt "1" ] && retval=$(_ncores-1)
+	echo ${_ncores}
+}
+
+echo "unsetting PYTHONPATH"
+unset PYTHONPATH
 cmake -S.. -Bbuild -DBUILD_PYTHON=ON -DCMAKE_INSTALL_PREFIX=$PWD/install -DCMAKE_BUILD_TYPE=Release \
-&& cmake --build build --target all -- -j \
+&& cmake --build build --target all -- -j $(n_cores) \
 && cmake --build build --target install
