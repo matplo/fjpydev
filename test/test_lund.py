0101aebf458161cd0c1c424eb799bd4c842dde97
Mateusz Ploskon <mploskon@lbl.gov>
2019-06-19 21:49:32 -0700

    more info printout

--- test/test_lund.py
+++ test/test_lund.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
-# coding: utf-8
 import fastjet as fj
 import pythia8
 from recursivetools import pyrecursivetools as rt
 from lundplane import pylundplane as lund
 from pythiafjtools import pypythiafjtools as pyfj
 import math
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
 from tqdm import tqdm
+
 
 def deltas(jets, jets0):
     for i in range(len(jets)):
         yield jets0[i].perp() - jets[i].perp()
+
 
 def create_and_init_pythia(config_strings=[]):
     pythia = pythia8.Pythia()
@@ -25,19 +23,17 @@ def create_and_init_pythia(config_strings=[]):
         return pythia
     return None
 
-sconfig_pythia = [ "Beams:eCM = 8000.", "HardQCD:all = on", "PhaseSpace:pTHatMin = 20."]
+
+sconfig_pythia = ["Beams:eCM = 8000.", "HardQCD:all = on", "PhaseSpace:pTHatMin = 100."]
 pythia = create_and_init_pythia(sconfig_pythia)
 
+# print the banner first
+fj.ClusterSequence.print_banner()
+print()
 # set up our jet definition and a jet selector
 jet_R0 = 0.4
 jet_def = fj.JetDefinition(fj.antikt_algorithm, jet_R0)
-selector = fj.SelectorPtMin(20.0) & fj.SelectorPtMax(40.0) & fj.SelectorAbsEtaMax(1)
-sd = rt.SoftDrop(0, 0.1, 1.0)
-
-# set up our jet definition and a jet selector
-jet_R0 = 0.4
-jet_def = fj.JetDefinition(fj.antikt_algorithm, jet_R0)
-jet_selector = fj.SelectorPtMin(20.0) & fj.SelectorPtMax(40.0) & fj.SelectorAbsEtaMax(1)
+jet_selector = fj.SelectorPtMin(100.0) & fj.SelectorPtMax(200.0) & fj.SelectorAbsEtaMax(1)
 sd = rt.SoftDrop(0, 0.1, 1.0)
 
 all_jets = []
@@ -56,11 +52,9 @@ nangs0 = [pyfj.angularity(j, 0.) for j in all_jets]
 jet_def_lund = fj.JetDefinition(fj.cambridge_algorithm, 1.0)
 lund_gen = lund.LundGenerator(jet_def_lund)
 
-# lunds = [lund_gen.result(j) for j in all_jets]
-# for l in lunds:
-#     print(l)
-
-for j in all_jets:
-    l = lund_gen.result(j)
-    for x in l:
-        print(x.kt())
+lunds = [lund_gen.result(j) for j in all_jets]
+print('listing lund plane points... Delta, kt - for {} selected jets'.format(len(all_jets)))
+for l in lunds:
+    print ('- jet pT={0:5.2f} eta={1:5.2f}'.format(l[0].pair().perp(), l[0].pair().eta()))
+    print ('  Deltas={}'.format([s.Delta() for s in l]))
+    print ('  kts={}'.format([s.Delta() for s in l]))
