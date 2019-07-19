8a1f33167241ef1096d56cfa9421c365363ac174
James Mulligan <james.mulligan@berkeley.edu>
2019-07-19 16:17:33 -0700

    change order of paths to take precedence over previously defined paths

--- test/setup_env.sh
+++ test/setup_env.sh
@@ -28,20 +28,20 @@ SCRIPTPATH=$(thisdir)
 # export PYTHONPATH=${PYTHONPATH}:${recursivetools}:${pyfastjet}
 
 python_version=$(python3 --version | cut -f 2 -d' ' | cut -f 1-2 -d.)
-export PYTHONPATH=${PYTHONPATH}:${FASTJET_DIR}/lib/python${python_version}/site-packages
-export PYTHONPATH=${PYTHONPATH}:${HEPMC2_DIR}/lib
-export PYTHONPATH=${PYTHONPATH}:${LHAPDF_DIR}/lib/python${python_version}/site-packages
-export PYTHONPATH=${PYTHONPATH}:${PYTHIA_DIR}/lib
-export PYTHONPATH=${PYTHONPATH}:${SCRIPTPATH}/build/python/fjpy
+export PYTHONPATH=${FASTJET_DIR}/lib/python${python_version}/site-packages:${PYTHONPATH}
+export PYTHONPATH=${HEPMC2_DIR}/lib:${PYTHONPATH}
+export PYTHONPATH=${LHAPDF_DIR}/lib/python${python_version}/site-packages:${PYTHONPATH}
+export PYTHONPATH=${PYTHIA_DIR}/lib:${PYTHONPATH}
+export PYTHONPATH=${SCRIPTPATH}/build/python/fjpy:${PYTHONPATH}
 
-export PATH=${PATH}:${HEPMC_DIR}/bin:${LHAPDF_DIR}/bin:${PYTHIA8_DIR}/bin:${FASTJET_DIR}/bin
+export PATH=${HEPMC_DIR}/bin:${LHAPDF_DIR}/bin:${PYTHIA8_DIR}/bin:${FASTJET_DIR}/bin:${PATH}
 if [ -z ${LD_LIBRARY_PATH} ]; then
 	export LD_LIBRARY_PATH=${HEPMC_DIR}/lib:${LHAPDF_DIR}/lib:${PYTHIA_DIR}/lib:${FASTJET_DIR}/lib
 else
-	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HEPMC_DIR}/lib:${LHAPDF_DIR}/lib:${PYTHIA_DIR}/lib:${FASTJET_DIR}/lib
+	export LD_LIBRARY_PATH=${HEPMC_DIR}/lib:${LHAPDF_DIR}/lib:${PYTHIA_DIR}/lib:${FASTJET_DIR}/lib:${LD_LIBRARY_PATH}
 fi
 if [ -z ${DYLD_LIBRARY_PATH} ]; then
 	export DYLD_LIBRARY_PATH=${HEPMC_DIR}/lib:${LHAPDF_DIR}/lib:${PYTHIA_DIR}/lib:${FASTJET_DIR}/lib
 else
-	export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${HEPMC_DIR}/lib:${LHAPDF_DIR}/lib:${PYTHIA_DIR}/lib:${FASTJET_DIR}/lib
+	export DYLD_LIBRARY_PATH=${HEPMC_DIR}/lib:${LHAPDF_DIR}/lib:${PYTHIA_DIR}/lib:${FASTJET_DIR}/lib:${DYLD_LIBRARY_PATH}
 fi
